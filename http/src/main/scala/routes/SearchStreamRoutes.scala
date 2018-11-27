1174de3652f6c95d139965fdfdf90899b73346cf
once-ler <htao@eztier.com>
2018-11-26 19:19:19 -0500

    Fix http response stream

--- http/src/main/scala/routes/SearchStreamRoutes.scala
+++ http/src/main/scala/routes/SearchStreamRoutes.scala
@@ -1,27 +1,26 @@
 package com.eztier.rest.routes
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.common
 import akka.http.scaladsl.common.EntityStreamingSupport
 import akka.http.scaladsl.model.ContentTypes._
 import akka.http.scaladsl.model.HttpEntity
-import akka.http.scaladsl.server.Directives._
+import akka.util.ByteString
+
+// akka execution context
 import akka.stream.scaladsl.{Flow, Source, Sink}
 import akka.stream.{ActorMaterializer, ThrottleMode}
-import akka.util.ByteString
-import com.eztier.postgres.eventstore.models.Patient
-import com.eztier.postgres.async.CommandRunner
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
-// spraj-json
-// import com.eztier.rest.responses.SearchPatientJsonProtocol._
-
-// import akka.http.scaladsl.server.Directives._
-// import de.heikoseeberger.akkahttpcirce._
+// akka-http-circe
+import akka.http.scaladsl.server.Directives._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.generic.auto._
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
+
+// doobie
+import com.eztier.postgres.eventstore.models.{Patient, CaPatient, Model}
+import com.eztier.postgres.async.CommandRunner
 
 // For testing
 case class Dummy(name: String)
@@ -30,6 +29,7 @@ trait SearchStreamRoutes {
   implicit val actorSystem: ActorSystem
   implicit val streamMaterializer: ActorMaterializer
   implicit val executionContext: ExecutionContext
+
   lazy val httpStreamingRoutes = streamingJsonRoute
   lazy val httpInfoStreamingRoutes = streamingInfoRoute
   lazy val httpStreamingSearchRoutes = streamingSearchRoute
@@ -66,38 +66,11 @@ trait SearchStreamRoutes {
       }
     }
     
-  def searchEventstore(term: String) = Flow[Patient].map {
-    s =>
-      CommandRunner.search[Patient](term)
-  }
-    
   /*
     @test
       curl -XPOST -H 'Content-Type:application/json'  -d '{"name": "abc"}' localhost:9000/search
   */
-  /*
-  def streamingSearchRoute =
-    path("search") {
-      post {
-        entity(as[Patient]) { p =>
-
-          val resp = Source.single(p)
-            .via(searchEventstore(p.name))
-            .throttle(elements = 100, per = 1 second, maximumBurst = 1, mode = ThrottleMode.Shaping)
-
-          // complete(HttpEntity(`text/plain(UTF-8)`, resp))
-          complete(resp)
-        }
-      }
-
-    }
-  */
-
   def streamingSearchRoute = {
-    import akka.http.scaladsl.server.Directives._
-    import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
-    import io.circe.generic.auto._
-  
     path("search") {
       get {
         val sourceOfNumbers = Source(1 to 15)
@@ -108,12 +81,10 @@ trait SearchStreamRoutes {
         complete(sourceOfSearchMessages)
       } ~ post {
         entity(as[Patient]) { p =>
-
-          val resp = Source.single(p)
-            .via(searchEventstore(p.name))
+          val resp = CommandRunner
+            .search[CaPatient](p.name)
             .throttle(elements = 100, per = 1 second, maximumBurst = 1, mode = ThrottleMode.Shaping)
             
-          // complete(HttpEntity(`text/plain(UTF-8)`, resp))
           complete(resp)
         }
       }
